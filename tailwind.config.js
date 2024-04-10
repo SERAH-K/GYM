6ec632ef0c6feb41e431d24b13e4106680250df0
Jasper Ngunjiri <jasperwambugu@gmail.com>
2024-04-10 19:34:27 +0300

    configured tailwind file, added a responsive navigation  bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.html',
-
-  ],
+  content: ['./*.html'] ,
   theme: {
     screens:{
-     sm:'480px',
-     md:'768px',
-     lg:'976px',
-     xl:'1440px'
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl:'1440px'
     },
-    fontFamily:{
-      
-      
-  
-    }
-    
-   
-    
-    extend: {},
+    extend: {
+      colors:{
+        brightRed:'hsl(12, 88%, 59%)',
+        brightRedLight:'hsl(12, 88%, 69%)',
+        brightRedSupLight:'hsl(12, 88%, 95%)',
+        darkblue:'hsl(228, 39%, 23%)',
+        darkGrayishBlue:'hsl(227, 12%, 61%)',
+        veryDarkBlue:'hsl(233, 12%, 13%)',
+        veryPaleRed:'hsl(13, 100%, 96%)',
+        veryLightGray:'hsl(0, 0%, 98%)',
+      }
+    },
   },
   plugins: [],
 }
